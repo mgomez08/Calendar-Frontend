be4bb59d0614ecb4dad867172197fed702269283
Marlon Gómez <marlonjulian15@gmail.com>
2021-09-10 09:59:44 -0500

    Use Loading Component

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,47 +1,44 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { 
-    BrowserRouter as Router,
-    Switch,
-    Redirect
-} from 'react-router-dom'
-import { startChecking } from '../actions/auth'
-import { LoginScreen } from '../components/auth/LoginScreen'
-import { CalendarScreen } from '../components/calendar/CalendarScreen'
-import { PrivateRoute } from './PrivateRoute'
-import { PublicRoute } from './PublicRoute'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { startChecking } from "../actions/auth";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { CalendarScreen } from "../components/calendar/CalendarScreen";
+import { Loading } from "../components/ui/Loading";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(startChecking());
+  }, [dispatch]);
+  if (checking) {
+    return <Loading />;
+  } else {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <PublicRoute
+              exact
+              path="/login"
+              component={LoginScreen}
+              isLoggedIn={!!uid}
+            />
+            <PrivateRoute
+              exact
+              path="/"
+              component={CalendarScreen}
+              isLoggedIn={!!uid}
+            />
 
-    const dispatch = useDispatch();
-    const { checking, uid } = useSelector(state => state.auth)
-    useEffect(() => {
-        dispatch( startChecking());
-    }, [dispatch])
-    if( checking ){
-        return <h5>Espere...</h5>
-    }else{
-        return (
-            <Router>
-                <div>
-                    <Switch>
-                        <PublicRoute exact
-                         path="/login" 
-                         component={LoginScreen} 
-                         isLoggedIn={ !!uid }                             
-                         />
-                        <PrivateRoute 
-                         exact 
-                         path="/" 
-                         component={CalendarScreen} 
-                         isLoggedIn={ !!uid }      
-                        />
-    
-                        <Redirect to="/" />
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
-}
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+};
