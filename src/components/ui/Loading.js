ca58344b9dc139ed92b6caca916bd789b369e90b
Marlon Gómez <marlonjulian15@gmail.com>
2021-09-10 09:57:24 -0500

    Structure Loading Component

---
+++ src/components/ui/Loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Loading = () => {
+  return (
+    <>
+      <div className="loading">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <h1>Cargando información</h1>
+      </div>
+    </>
+  );
+};
